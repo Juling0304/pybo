76856c094d1242f80aeda49e6be70f9a228d44a4
com928 <com928@naver.com>
2023-07-21 17:08:22 +0900

    fix: 230721 오후

--- frontend/src/routes/Detail.svelte
+++ frontend/src/routes/Detail.svelte
@@ -3,13 +3,14 @@
     import Error from "../components/Error.svelte"
     import { link, push } from 'svelte-spa-router'
     import { is_login, username, modify_answer_id } from "../lib/store"
+    import { marked } from 'marked'
     import moment from 'moment/min/moment-with-locales'
     moment.locale('ko')
 
     export let params = {}
     
     let question_id = params.question_id
-    let question = {answers:[]}
+    let question = {answers:[], voter:[], content: ''}
     let content = ""
     let error = {detail:[]}
     $modify_answer_id = ""
@@ -71,6 +72,40 @@
         }
     }
 
+    function vote_question(_question_id) {
+        if(window.confirm('정말로 추천하시겠습니까?')) {
+            let url = "/api/question/vote"
+            let params = {
+                question_id: _question_id
+            }
+            fastapi('post', url, params, 
+                (json) => {
+                    get_question()
+                },
+                (err_json) => {
+                    error = err_json
+                }
+            )
+        }
+    }
+
+    function vote_answer(answer_id) {
+        if(window.confirm('정말로 추천하시겠습니까?')) {
+            let url = "/api/answer/vote"
+            let params = {
+                answer_id: answer_id
+            }
+            fastapi('post', url, params, 
+                (json) => {
+                    get_question()
+                },
+                (err_json) => {
+                    error = err_json
+                }
+            )
+        }
+    }
+
     get_question()
 </script>
 
@@ -79,7 +114,9 @@
     <h2 class="border-bottom py-2">{question.subject}</h2>
     <div class="card my-3">
         <div class="card-body">
-            <div class="card-text" style="white-space: pre-line;">{question.content}</div>
+            <div class="card-text">
+                {@html marked.parse(question.content)}
+            </div>
             <div class="d-flex justify-content-end">
                 {#if question.modify_date }
                 <div class="badge bg-light text-dark p-2 text-start mx-3">
@@ -93,6 +130,11 @@
                 </div>
             </div>
             <div class="my-3">
+                <button class="btn btn-sm btn-outline-secondary"
+                    on:click="{vote_question(question.id)}"> 
+                    추천
+                    <span class="badge rounded-pill bg-success">{ question.voter.length }</span>
+                </button>
                 {#if question.user && $username === question.user.username }
                 <a use:link href="/question-modify/{question.id}" 
                     class="btn btn-sm btn-outline-secondary">수정</a>
@@ -110,7 +152,9 @@
     {#each question.answers as answer}
     <div class="card my-3">
         <div class="card-body">
-            <div class="card-text" style="white-space: pre-line;">{answer.content}</div>
+            <div class="card-text">
+                {@html marked.parse(answer.content)}
+            </div>
             <div class="d-flex justify-content-end">
                 {#if answer.modify_date }
                 <div class="badge bg-light text-dark p-2 text-start mx-3">
@@ -124,6 +168,11 @@
                 </div>
             </div>
             <div class="my-3">
+                <button class="btn btn-sm btn-outline-secondary"
+                    on:click="{vote_answer(answer.id)}"> 
+                    추천
+                    <span class="badge rounded-pill bg-success">{ answer.voter.length }</span>
+                </button>
                 {#if answer.user && $username === answer.user.username }
                 <a on:click="{() => $modify_answer_id = answer.id}"
                      use:link href="/answer-modify" 
